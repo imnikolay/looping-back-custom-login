9a1d9b87a2e4b08e1169f47c9c0904774085819e
Nikolay Mihaylov <nm@nomadcy.com>
2022-06-29 11:16:43 +0300

    add login logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {useAuth0} from "@auth0/auth0-react";
 
 function App() {
+  const {logout, loginWithRedirect} = useAuth0()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <button
+      onClick={() => loginWithRedirect()}
+      >
+        Login
+      </button>
+        <button
+        onClick={() => logout()}
         >
-          Learn React
-        </a>
-      </header>
+            Logout
+        </button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider
+        domain="dev-ytwscsex.us.auth0.com"
+        clientId="OhjTliOUABJwmRrVXwHCn16kSYdzpqAu"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
